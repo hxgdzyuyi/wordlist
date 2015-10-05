bb1f2f73212247f3a18c98764666986209fdd682
hxgdzyuyi <hxgdzyuyi@gmail.com>
2015-10-05 17:41:26 +0800

    添加 Word 对象，管理英文单词的翻译
    翻译的 (英文-中文) 字典文件
    Copy from https://github.com/solos/regexdict/blob/master/src/regexdict/dict.json
    来自星际译王

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+def create_dictionary_info
+  dict_file = File.expand_path('../fixtures/dict.json', __FILE__)
+  parsed_dict = JSON.parse(File.read(dict_file))
+  r_remove_pronounce = /^\[.*\]\s+\n\s/
+
+  Word.delete_all
+
+  Word.create(
+    parsed_dict.map do |word, definition|
+      { word: word, definition: definition.gsub(r_remove_pronounce, '') }
+    end
+  )
+end
+
+create_dictionary_info()
